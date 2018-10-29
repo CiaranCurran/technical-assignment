bec9dcfab038038e23fcc1b3562ef00c17b630d9
Ciaran Curran <ciaran.curran@ucdconnect.ie>
2018-10-29 21:16:05 +0000

    Valid form submition returns the word count and redirects to index.html

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
 
-class wordCountForm(FlaskForm):
+class WordCountForm(FlaskForm):
     word = StringField('Word to be counted...', validators=[DataRequired()])
     url = StringField('URL to be searched...', validators=[DataRequired(), URL()])
     submit = SubmitField('Count Word')
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,19 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import WordCountForm
+from app.scrape import countWord
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Ciarann'}
-    return render_template('index.html', title='Word Count App', user=user)
+    return render_template('index.html', title='Word Count App')
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    form = WordCountForm()
+    if form.validate_on_submit():
+        count = countWord(form.word.data, form.url.data)
+        flash('The word "{}" appears in {} {} times'.format(
+            form.word.data, form.url.data, count))
+        return redirect(url_for('index'))
+    return render_template('form.html', title='Word Count App', form=form)
