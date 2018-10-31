96b2d105af54e9a886aca827e3efa4fb9909947e
Ciaran Curran <ciaran.curran@ucdconnect.ie>
2018-10-31 19:24:11 +0000

    fixed html parsing

--- app/routes.py
+++ app/routes.py
@@ -14,11 +14,11 @@ def form():
     if form.validate_on_submit():
         scraped = Scraper(form.word.data, form.url.data)
         message = ""
-        message += 'The word "{}" appears in {} {} times'.format(
+        message += 'The word "{}" appears in {} {} times<br><br>'.format(
             scraped.word, scraped.url, scraped.countWord())
 
         for sentence, polarity in scraped.getSentiments().items():
-            message += '/n {} : {}'.format(sentence, polarity)
+            message += '{} : {} <br><br>'.format(sentence, polarity)
 
         return render_template('form.html', title='Word Count App', form=form, message=message)
     return render_template('form.html', title='Word Count App', form=form)
--- app/scrape.py
+++ app/scrape.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from bs4.element import Comment
 import requests
 from textblob import TextBlob
 
@@ -8,8 +9,7 @@ class Scraper:
         self.url = url
 
         source = requests.get(url).text
-        soup = BeautifulSoup(source, 'lxml')
-        self.text = soup.get_text()
+        self.text = text_from_html(source)
 
     def countWord(self):
         count = self.text.count(self.word)
@@ -31,3 +31,16 @@ class Scraper:
         return sentiments
 
 
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+
+def text_from_html(body):
+    soup = BeautifulSoup(body, 'html.parser')
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)
+    return u" ".join(t.strip() for t in visible_texts)
